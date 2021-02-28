f801b3695ea2a24e1713271f61090d73d8924874
Michele Amante <michele.amante@capgemini.com>
2021-03-01 00:27:28 +0100

    Code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
-import React, { useEffect, useState } from "react";
-
-import ProductListView from "./components/product-list/product-list";
-import { ProductItemList } from "./model/products/product-item-list";
-import { ProductManager } from "./modules/product-manager/product-manager";
+import ProductPage from "./container/product-page/product-page";
 
 const App: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [productList, setProductList] = useState(new ProductItemList());
-
-  useEffect(() => {
-    if(!isLoading && !productList.items) {
-      setIsLoading(true);
-      ProductManager.getProductList().then((result) => {
-        setIsLoading(false);
-        if(result.items != null && JSON.stringify(result.items) !== JSON.stringify(productList)) {
-          setProductList(result);
-        }
-      });
-    }
-  });
-
   return (
-    <div className="product-page">
-      <header className="header">
-        <div>menu</div>
-      </header>
-      <ProductListView isLoading={isLoading} list={productList} />
-      <footer className="footer">
-        <div>footer</div>
-      </footer>
-    </div>
+    <ProductPage />
   );
 }
 
